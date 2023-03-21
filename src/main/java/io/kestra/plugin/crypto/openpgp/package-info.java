de936586a62e22c51a3186d07d267f99d7d0544a
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:28:00 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/crypto/openpgp/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for encrypting and decrypting files.",
+    categories = PluginSubGroup.PluginCategory.TRANSFORMATION
+)
+package io.kestra.plugin.crypto.openpgp;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
